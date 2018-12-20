1a225edf4013f9c30eba9f164cc9fe9e086a7049
liuyujia <417378336@qq.com>
2018-12-20 11:06:55 +0800

    Initial Commit

---
+++ NextResponderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NextResponderDemo
+//
+//  Created by liuyujia on 2018/12/20.
+//  Copyright © 2018 liuyujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NextResponderDemoTests/NextResponderDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  NextResponderDemoTests.m
+//  NextResponderDemoTests
+//
+//  Created by liuyujia on 2018/12/20.
+//  Copyright © 2018 liuyujia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NextResponderDemoTests : XCTestCase
+
+@end
+
+@implementation NextResponderDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
